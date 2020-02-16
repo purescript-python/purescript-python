ee6c17034e759770a74209975078043e45f95365
thautawarm <twshere@outlook.com>
2020-02-16 16:43:48 +0900

    fix implementation

--- app/Main.hs
+++ app/Main.hs
@@ -128,8 +128,8 @@ loaderCode =
     T.unlines . map T.pack $
       [
         "from purescripto import LoadPureScript"
-      , "_py__ = globals()"
-      , "_ps__ = LoadPureScript(__file__, __name__)"
+      , "__py__ = globals()"
+      , "__ps__ = LoadPureScript(__file__, __name__)"
       , "__all__ = list(__ps__)"
       , "__py__.update(__ps__)"
       ]      

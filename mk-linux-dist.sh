918585476615143bd2974a64f4314ae1c3056f87
thautawarm <twshere@outlook.com>
2020-02-22 01:42:37 +0900

    remove incorrect packaging

--- mk-linux-dist.sh
+++
@@ -1,7 +0,0 @@
-# NOTE: This packaging script tested only on Arch/Manjaro Linux.
-python mk-linux-dist.py
-cd linux-libs
-chrpath -r "./" pspy-blueprint
-cd ..
-zip -r pspy-blueprint-linux.zip linux-libs
-rm -rf linux-libs

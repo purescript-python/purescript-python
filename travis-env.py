5ed13b03bb32e88e204a7bc789780499b9715edd
thautawarm <twshere@outlook.com>
2020-02-24 16:44:12 +0900

    up

--- travis-env.py
+++ travis-env.py
@@ -1,16 +1,10 @@
-from base64 import b32decode
 import sys
 import os
-
-def auth():
-    AUTH = os.environ['RELEASE_AUTH']
-    sys.stdout.write(b32decode(AUTH.encode('utf8')).decode('utf8').strip())
 
 def platform():
     from distutils.util import get_platform
     sys.stdout.write(get_platform())
 
 {
-    'auth': auth,
     'plat': platform
 }[sys.argv[1]]()
--- travis-env.sh
+++ travis-env.sh
@@ -1,5 +1,4 @@
 echo "==================setting travis release uploading environment================"
-export RELEASE_AUTH_KEY="`python travis-env.py auth`"
 export DIST_FILE="`stack path --dist-dir`/build/pspy-blueprint/pspy-blueprint"
 cp $DIST_FILE ./pspy-blueprint
 export RELEASE_TAG="`python travis-env.py plat`"

e47063a1614e43c710ee701215d76d05a78c12b8
thautawarm <twshere@outlook.com>
2020-02-24 16:04:11 +0900

    auto release tag

--- travis-env.sh
+++ travis-env.sh
@@ -2,5 +2,6 @@ echo "==================setting travis release uploading environment============
 export RELEASE_AUTH_KEY="`python travis-env.py auth`"
 export DIST_FILE="`stack path --dist-dir`/build/pspy-blueprint/pspy-blueprint"
 cp $DIST_FILE ./pspy-blueprint
+export RELEASE_TAG="`python travis-env.py plat`"
 export ZIP_FILE="pspy-blueprint-`python travis-env.py plat`.zip"
 zip -r $ZIP_FILE . -x "*.git*" "*.stack-work*"

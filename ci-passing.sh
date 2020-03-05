36248b65bcd160dc8707a463d65b23314a263917
thautawarm <twshere@outlook.com>
2020-03-05 22:42:15 +0900

    release v0.1.2 & build binaries from ci

--- ci-passing.sh
+++ ci-passing.sh
@@ -1,8 +1,13 @@
 cd passing
+
 nvm install 12.16.1
 nvm use 12.16.1
+
 npm install -g purescript
 npm install -g spago
 
+# this allows failure
+pyenv global 3.7
 pip3 install "purescripto>=0.8.0,<0.9.0"
+
 spago test

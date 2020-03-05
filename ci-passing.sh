20b9bb2ca4d86782039799cdf40cfa6dfb5f2384
thautawarm <twshere@outlook.com>
2020-03-05 21:36:48 +0900

    ci: fix linux python env

--- ci-passing.sh
+++ ci-passing.sh
@@ -3,5 +3,6 @@ nvm install 12.16.1
 nvm use 12.16.1
 npm install -g purescript
 npm install -g spago
-pip3 install "purescripto>=0.8.0,<0.9.0"
+pyenv global 3.7
+pip install "purescripto>=0.8.0,<0.9.0"
 spago test

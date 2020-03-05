af5f9030843c24b0d38f4c2a960b2fc03cc12f1e
thautawarm <twshere@outlook.com>
2020-03-05 22:26:25 +0900

    ci

--- ci-passing.sh
+++ ci-passing.sh
@@ -4,7 +4,5 @@ nvm use 12.16.1
 npm install -g purescript
 npm install -g spago
 
-# if travis linux, install 3.7. otherwise it exists
-pyenv global 3.7 || echo "using system python shall be okay for OSX"
 pip3 install "purescripto>=0.8.0,<0.9.0"
 spago test

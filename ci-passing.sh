b60f8e304f8a5dc54414c7a08582d8bbb2dbb72b
thautawarm <twshere@outlook.com>
2020-03-05 22:14:04 +0900

    ci: try to install py3.7 via pyenv under 'python=3.7' travis env

--- ci-passing.sh
+++ ci-passing.sh
@@ -4,7 +4,6 @@ nvm use 12.16.1
 npm install -g purescript
 npm install -g spago
 
-pyenv install 3.7.4
-pyenv global 3.7.4
+pyenv install 3.7.4 && pyenv global 3.7.4 || pyenv global 3.7
 pip install "purescripto>=0.8.0,<0.9.0"
 spago test

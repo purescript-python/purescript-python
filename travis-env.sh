7cb9482c83bc0b15f489152f397a850555638065
thautwarm <twshere@outlook.com>
2020-06-13 16:58:42 +0900

    avoid packing passing directories

--- travis-env.sh
+++ travis-env.sh
@@ -3,4 +3,4 @@ export DIST_FILE="`stack path --dist-dir`/build/pspy-blueprint/pspy-blueprint"
 cp $DIST_FILE ./pspy-blueprint
 export RELEASE_TAG="`python travis-env.py plat`"
 export ZIP_FILE="pspy-blueprint-`python travis-env.py plat`.zip"
-zip -r $ZIP_FILE . -x "*.git*" "*.stack-work*"
+zip -r $ZIP_FILE . -x "*.git*" "*.stack-work*" "passing/*"
